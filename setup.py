094a8c2352f5295385112f8ce17be79306271594
Jesse <jesse.gresock@gmail.com>
2025-02-22 13:22:35 -0500

    updated unittest and added setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="benchy",
+    version="1.0",
+    packages=find_packages(),
+    author = "Gresock, Jesse",
+    description="Simple benchmarking tool",
+    url = "https://github.com/jessegresock/benchy",
+    python_requires=">=3.6"
+)
--- tests/cases/test_functions.py
+++ tests/cases/test_functions.py
@@ -4,14 +4,17 @@ from benchy import functions
 
 class TestFunctions(unittest.TestCase):
 
+    def multiply(self, a, b):
+        return a * b
+
     def test_positive_timer(self):
         """Test measure_time returns only a positive timing value"""
-        result, time = functions.measure_time(print, 'Hello, World!')
-        self.assertEqual(time > 0)
+        result, time = functions.measure_time(self.multiply, 1, 2)
+        self.assertTrue(time > 0)
 
     def test_tuple_return(self):
         """Test measure_time return type is of type tuple"""
-        ttuple = functions.measure_time(print, "Hello, World!")
+        ttuple = functions.measure_time(self.multiply, 1, 2)
         self.assertIsInstance(ttuple, tuple)
 
 
